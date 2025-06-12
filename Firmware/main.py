fee65c2397daff6e8e640238b16873dc222bfb20
Ayush J <ayushjaiswal18.2009@gmail.com>
2025-06-12 18:28:09 -0400

    adwa

--- Firmware/main.py
+++ Firmware/main.py
@@ -1,33 +1,36 @@
 import board
-# These are imports from the kmk library
 from kmk.kmk_keyboard import KMKKeyboard
-from kmk.scanners.keypad import KeysScanner
+from kmk.scanners import DiodeOrientation
+from kmk.scanners.encoder import RotaryEncoder
 from kmk.keys import KC
-from kmk.modules.macros import Press, Release, Tap, Macros
+from kmk.modules.layers import Layers
 
-# This is the main instance of your keyboard
 keyboard = KMKKeyboard()
 
-# Add the macro extension
-macros = Macros()
-keyboard.modules.append(macros)
+keyboard.col_pins = (board.D0, board.D1, board.D2, board.D3, board.D4)
+keyboard.row_pins = (board.D5, board.D6)
+keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
-# Define your pins here!
-PINS = [board.D3, board.D4, board.D2, board.D1]
-
-# Tell kmk we are not using a key matrix
-keyboard.matrix = KeysScanner(
-    pins=PINS,
-    value_when_pressed=False,
+encoder = RotaryEncoder(
+    pins=(board.D7, board.D8),
+    resolution=4,
+    pos_side=KC.VOLU,
+    neg_side=KC.VOLD
 )
+keyboard.modules.append(encoder)
 
-# Here you define the buttons corresponding to the pins
-# Look here for keycodes: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/keycodes.md
-# And here for macros: https://github.com/KMKfw/kmk_firmware/blob/main/docs/en/macros.md
+keyboard.encoder_pins = (board.D9,)
+keyboard.encoder_map = [(KC.MUTE,)]
+
+layers = Layers()
+keyboard.modules.append(layers)
+
 keyboard.keymap = [
-    [KC.A, KC.DELETE, KC.MACRO("Hello world!"), KC.Macro(Press(KC.LCMD), Tap(KC.S), Release(KC.LCMD)),]
+    [
+        KC.ESC, KC.Q, KC.W, KC.E, KC.BSPACE,
+        KC.LSHIFT,   KC.CAPSLOCK, KC.D, KC.F, KC.ENTER
+    ]
 ]
 
-# Start kmk!
 if __name__ == '__main__':
     keyboard.go()
