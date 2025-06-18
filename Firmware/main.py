244a467634f2498f0fd97b6d5ca77dec8de9f5b9
ayush jaiswal <ayushjaiswal18.2009@gmail.com>
2025-06-17 21:13:15 -0400

    updated code for rgb support

--- Firmware/main.py
+++ Firmware/main.py
@@ -1,35 +1,49 @@
 import board
+
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.scanners import DiodeOrientation
-from kmk.scanners.encoder import RotaryEncoder
+from kmk.modules.rgb import RGB
+from kmk.modules.encoder import EncoderHandler
 from kmk.keys import KC
-from kmk.modules.layers import Layers
 
 keyboard = KMKKeyboard()
-
 keyboard.col_pins = (board.D0, board.D1, board.D2, board.D3, board.D4)
 keyboard.row_pins = (board.D5, board.D6)
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
-encoder = RotaryEncoder(
-    pins=(board.D7, board.D8),
-    resolution=4,
-    pos_side=KC.VOLU,
-    neg_side=KC.VOLD
-)
-keyboard.modules.append(encoder)
-
-keyboard.encoder_pins = (board.D9,)
-keyboard.encoder_map = [(KC.MUTE,)]
-
-layers = Layers()
-keyboard.modules.append(layers)
 
 keyboard.keymap = [
     [
-        KC.ESC, KC.TAB, KC.CAPSLOCK, KC.BSPACE, KC.DELETE,
-        KC.LSHIFT,KC.LCTRL, KC.LALT, KC.F, KC.ENTER
+        KC.LCTL(KC.S),       # save stuff
+        KC.LCTL(KC.Z),       # undo 
+        KC.LCTL(KC.Y),       # redo
+        KC.LCTL(KC.P),       # file search
+        KC.LCTL(KC.Q),       # quick switch
+        KC.LCTL(KC.R),       # switch projects
+        KC.LCTL(KC.GRAVE),   # Terminal
+        KC.LCTL(KC.LSFT(KC.M)), # problems panel
+        KC.LCTL(KC.LSFT(KC.P)), # command palette
+        KC.LCTL(KC.J),       # toggle panel
     ]
+]
+
+rgb = RGB(
+    pixel_pin=board.D9,          
+    num_pixels=10,               
+    hue_default=0,               
+    sat_default=100,
+    val_default=100,
+    val_limit=255,
+    animation_mode=2            
+)
+keyboard.modules.append(rgb)
+
+encoder = EncoderHandler()
+keyboard.modules.append(encoder)
+
+encoder.pins = ((board.D7, board.D8, board.D9),)
+encoder.map = [
+    ((KC.VOLD, KC.VOLU, KC.MUTE),)       
 ]
 
 if __name__ == '__main__':
